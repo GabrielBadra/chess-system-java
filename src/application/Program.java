c5e21668a0a0705d63e7f9454ae333a4a430725f
GabrielBadra <gabrielfreitasalves9@gmail.com>
2023-07-05 19:45:13 -0300

    Move pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,31 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.CheesMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		CheesMatch chessMatch = new CheesMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while(true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
+		
 
 	}
 
