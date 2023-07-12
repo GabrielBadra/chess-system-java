69b9a21f22bffbb3270617f7bfc2d4e18cd21918
GabrielBadra <gabrielfreitasalves9@gmail.com>
2023-07-11 22:44:52 -0300

    Checkmate logic

--- src/chess/CheesMatch.java
+++ src/chess/CheesMatch.java
@@ -16,6 +16,7 @@ public class CheesMatch {
 	private int turn;
 	private Color currentPlayer;
 	private boolean check;
+	private boolean checkMate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -36,6 +37,9 @@ public class CheesMatch {
 	}
 	public boolean getCheck() {
 		return check;
+	}
+	public boolean getCheckMate() {
+		return checkMate;
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -65,7 +69,11 @@ public class CheesMatch {
 		}
 		
 		check = (testCheck(opponent(currentPlayer)))? true : false;
-		nextTurn();
+		if(testCheckMate(opponent(currentPlayer))) {
+			checkMate = true;
+		}else {
+			nextTurn();
+		}
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -141,6 +149,33 @@ public class CheesMatch {
 			}
 		}
 		return false;
+	}
+	private boolean testCheckMate(Color color) {
+		if(!testCheck(color)) {
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+		
+		for(Piece p: list) {
+			boolean[][] mat = p.possibleMoves();
+			for(int i=0;i < board.getRows(); i++) {
+				for(int j = 0; j < board.getColumns(); j++) {
+					if(mat[i][j]) {
+						Position source = ((ChessPiece)p).getChessPosition().toPosition();
+						Position target = new Position(i, j);
+						
+						Piece capturedPiece = makeMove(source, target);
+						boolean testCheck = testCheck(color);
+						undoMove(source, target, capturedPiece);
+						
+						if(!testCheck) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
